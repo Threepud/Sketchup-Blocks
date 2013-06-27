4e83226b17b34585d522014c587952054b98ccdd
Elre <vze.ezv@gmail.com>
2013-06-27 18:30:58 +0200

    Added SessionManager's private member variables

--- EclipseProject/SketchupBlocks/src/SessionManager.java
+++ EclipseProject/SketchupBlocks/src/SessionManager.java
@@ -3,6 +3,10 @@ import processing.core.PApplet;
 class SessionManager
 {
 	private PApplet parent;
+	private ModelConstructor jimmy;
+	private Exporter kreshnik;
+	private ModelViewer sarah;
+	private BlockDatabase blockDB;
 	
 	public SessionManager(PApplet _parent)
 	{
@@ -12,21 +16,23 @@ class SessionManager
     public void onCameraEvent(CameraEvent cameraEvent)
     {
     	parent.println("Received camera event");
+    	//We still require some sort of menu traversal scheme...
+    	
     }
     
-    public void setModelConstructor(ModelConstructor jimmy)
+    public void setModelConstructor(ModelConstructor _jimmy)
     {
-      
+    	jimmy = _jimmy;
     }
     
-    public void setExporter(Exporter kreshnik)
+    public void setExporter(Exporter _kreshnik)
     {
-      
+    	kreshnik = _kreshnik;
     }
     
-    public void setModelViewer(ModelViewer sarah)
+    public void setModelViewer(ModelViewer _sarah)
     {
-      
+    	sarah = _sarah;
     }
     
     public void loadProject()
