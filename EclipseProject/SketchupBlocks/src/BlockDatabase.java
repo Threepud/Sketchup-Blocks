c39cbbcb4e5680e301be5ae36a63c3414de4d748
Jacques Coetzee <jacqueslctz@gmail.com>
2013-06-27 15:07:53 +0200

    Further changes to constructor

--- EclipseProject/SketchupBlocks/src/BlockDatabase.java
+++ EclipseProject/SketchupBlocks/src/BlockDatabase.java
@@ -1,31 +1,40 @@
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.ObjectInputStream;
-import java.nio.file.Path;
 import java.util.ArrayList;
-//>EDITED
+
 class BlockDatabase
 {
-  private ArrayList<Block> blocks;
-  
-  BlockDatabase()
-  {
-    //try to deserialize the block database
-   /* FileInputStream fileInputSteam = new FileInputStream(new File("blockDB.dat"));
-    ObjectInputStream objectInputStream = new ObjectInputStream(fileInputSteam);
-    blocks = (ArrayList)objectInputStream.readObject();
-    objectInputStream.close();*/
-    
-    //throw exception if no database is found
-  }
-  
-  public void insertBlock(Block block)
-  {
-   
-  }
-   
-  public Block findBlock(int fiducialID)
-  {
-    return null;
-  } 
+	private ArrayList<Block> blocks;
+	  
+	BlockDatabase()
+	{
+		//try to deserialize the block database
+		FileInputStream fileInputSteam;
+		ObjectInputStream objectInputStream;
+		try 
+		{
+			fileInputSteam = new FileInputStream(new File("blockDB.dat"));
+			objectInputStream = new ObjectInputStream(fileInputSteam);
+			blocks = (ArrayList)objectInputStream.readObject();
+			objectInputStream.close();
+		}
+		catch (FileNotFoundException e) 
+		{
+			System.out.println(e);
+		}
+		
+		//throw exception if no database is found
+	}
+	  
+	public void insertBlock(Block block)
+	{
+	   
+	}
+	   
+	public Block findBlock(int fiducialID)
+	{
+		return null;
+	} 
 }
