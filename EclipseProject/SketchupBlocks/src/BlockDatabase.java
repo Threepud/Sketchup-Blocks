c163994139fae90dccf49c3a02cc6217a5464675
Jacques Coetzee <jacqueslctz@gmail.com>
2013-06-27 18:05:42 +0200

    Added loading functionality

--- EclipseProject/SketchupBlocks/src/BlockDatabase.java
+++ EclipseProject/SketchupBlocks/src/BlockDatabase.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
 
 class BlockDatabase
@@ -6,7 +10,7 @@ class BlockDatabase
 	private String smartBlockPath;
 	private String commandBlockPath;
 	private String userBlockPath;
-	  
+	
 	BlockDatabase(String _smartBlockPath, String _commandBlockPath, String _userBlockPath)
 	{
 		smartBlockPath = _smartBlockPath;
@@ -24,9 +28,9 @@ class BlockDatabase
 		boolean userBlockDatabaseFound = false;
 		
 		//load all block data into hash map
-		smartBlockDatabaseFound = loadSmartBlockData(smartBlockPath);
-		commandBlockDatabaseFound = loadCommandBlockData(commandBlockPath);
-		userBlockDatabaseFound = loadUserBlockData(userBlockPath);
+		smartBlockDatabaseFound = loadBlockData(smartBlockPath);
+		commandBlockDatabaseFound = loadBlockData(commandBlockPath);
+		userBlockDatabaseFound = loadBlockData(userBlockPath);
 		
 		//throw exception if no database is found
 		if(!smartBlockDatabaseFound)
@@ -37,37 +41,248 @@ class BlockDatabase
 			throw new DataBaseNotFoundException("");
 	}
 	
-	private boolean loadSmartBlockData(String fileName)
+	private boolean loadBlockData(String fileName)
 	{
+		BufferedReader bufferedReader;
+		String line;
+		try 
+		{
+			bufferedReader = new BufferedReader(new FileReader(fileName));
+			
+			while((line = bufferedReader.readLine()) != null)
+			{
+				if(fileName.equals(smartBlockPath))
+					loadSmartBlockLine(line);
+				else if(fileName.equals(commandBlockPath))
+					loadCommandBlockLine(line);
+				else if(fileName.equals(userBlockPath))
+					loadUserBlockLine(line);
+			}
+			
+			bufferedReader.close();
+		}
+		catch (FileNotFoundException e) 
+		{
+			e.printStackTrace();
+			return false;
+		} 
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+			return false;
+		}
 		
+		return true;
+	}
+	
+	private void loadSmartBlockLine(String line) throws RuntimeException
+	{
+		String[] elements = line.split("\t");
 		
-		return false;
+		if(elements.length != 4)
+			throw new RecordFormatException("Smart block database record length exception.");
+		
+		SmartBlock tempBlock = new SmartBlock();
+		
+		//block ID
+		try
+		{
+			tempBlock.blockId = Integer.parseInt(elements[0]);
+		}
+		catch(NumberFormatException e)
+		{
+			throw new RecordFormatException("Smart block database record block ID format exception.");
+		}
+		
+		//Associated Fiducial IDs
+		String[] stringAssociatedFiducials = elements[1].split(",");
+		int[] associatedFiducials = new int[stringAssociatedFiducials.length];
+		for(int x = 0; x < associatedFiducials.length; ++x)
+		{
+			try
+			{
+				associatedFiducials[x] = Integer.parseInt(stringAssociatedFiducials[x]);
+			}
+			catch(NumberFormatException e)
+			{
+				throw new RecordFormatException("Smart block database record associated fiducials format exception.");
+			}
+		}
+		tempBlock.associatedFiducials = associatedFiducials;
+		
+		//Block Vertices
+		String[] stringVertices = elements[2].split(",");
+		float[] vertices = new float[stringVertices.length];
+		for(int x = 0; x < vertices.length; ++x)
+		{
+			try
+			{
+				vertices[x] = Float.parseFloat(stringVertices[x]);
+			}
+			catch(NumberFormatException e)
+			{
+				throw new RecordFormatException("Smart block database record block vertices format exception.");
+			}
+		}
+		tempBlock.vertices = vertices;
+		
+		//Block indices
+		String[] stringIndices = elements[3].split(",");
+		int[] indices = new int[stringIndices.length];
+		for(int x = 0; x < indices.length; ++x)
+		{
+			try
+			{
+				indices[x] = Integer.parseInt(stringIndices[x]);
+			}
+			catch(NumberFormatException e)
+			{
+				throw new RecordFormatException("Smart block database record block indices format exception.");
+			}
+		}
+		tempBlock.indices = indices;
+		
+		//add block to hash for all associated fiducials
+		for(int x = 0; x < associatedFiducials.length; ++x)
+		{
+			blocks.put(new Integer(associatedFiducials[x]), tempBlock);
+		}
 	}
 	
-	private boolean loadCommandBlockData(String fileName)
+	private void loadCommandBlockLine(String line) throws RuntimeException
 	{
-		return false;
+		String[] elements = line.split("\t");
+		
+		if(elements.length != 3)
+			throw new RecordFormatException("Command block database record length exception.");
+		
+		CommandBlock tempBlock = new CommandBlock();
+		
+		//block ID
+		try
+		{
+			tempBlock.blockId = Integer.parseInt(elements[0]);
+		}
+		catch(NumberFormatException e)
+		{
+			throw new RecordFormatException("Command block database record block ID format exception.");
+		}
+		
+		//Associated Fiducial IDs
+		String[] stringAssociatedFiducials = elements[1].split(",");
+		int[] associatedFiducials = new int[stringAssociatedFiducials.length];
+		for(int x = 0; x < associatedFiducials.length; ++x)
+		{
+			try
+			{
+				associatedFiducials[x] = Integer.parseInt(stringAssociatedFiducials[x]);
+			}
+			catch(NumberFormatException e)
+			{
+				throw new RecordFormatException("Command block database record associated fiducials format exception.");
+			}
+		}
+		tempBlock.associatedFiducials = associatedFiducials;
+		
+		//command block type
+		CommandBlock.CommandType commandType;
+		switch(elements[2])
+		{
+			case "NEW":
+				commandType = CommandBlock.CommandType.NEW;
+				break;
+			case "SAVE":
+				commandType = CommandBlock.CommandType.SAVE;
+				break;
+			case "LOAD":
+				commandType = CommandBlock.CommandType.LOAD;
+				break;
+			case "EXPORT":
+				commandType = CommandBlock.CommandType.EXPORT;
+				break;
+			case "SPECTATE":
+				commandType = CommandBlock.CommandType.SPECTATE;
+				break;
+			default:
+				throw new RecordFormatException("Command block database record command type exception.");
+		}
+		tempBlock.type = commandType;
+		
+		//add block to hash for all associated fiducials
+		for(int x = 0; x < associatedFiducials.length; ++x)
+		{
+			blocks.put(new Integer(associatedFiducials[x]), tempBlock);
+		}
 	}
 	
-	private boolean loadUserBlockData(String fileName)
+	//TODO: validate user information
+	private void loadUserBlockLine(String line) throws RuntimeException
 	{
-		return false;
+		String[] elements = line.split("\t");
+		
+		if(elements.length != 5)
+			throw new RecordFormatException("User block database record length exception.");
+		
+		UserBlock tempBlock = new UserBlock();
+		
+		//block ID
+		try
+		{
+			tempBlock.blockId = Integer.parseInt(elements[0]);
+		}
+		catch(NumberFormatException e)
+		{
+			throw new RecordFormatException("User block database record block ID format exception.");
+		}
+		
+		//Associated Fiducial IDs
+		String[] stringAssociatedFiducials = elements[1].split(",");
+		int[] associatedFiducials = new int[stringAssociatedFiducials.length];
+		for(int x = 0; x < associatedFiducials.length; ++x)
+		{
+			try
+			{
+				associatedFiducials[x] = Integer.parseInt(stringAssociatedFiducials[x]);
+			}
+			catch(NumberFormatException e)
+			{
+				throw new RecordFormatException("User block database record associated fiducials format exception.");
+			}
+		}
+		tempBlock.associatedFiducials = associatedFiducials;
+		
+		//user name
+		tempBlock.name = elements[2];
+		
+		//user address
+		tempBlock.address = elements[3];
+		
+		//user picture path
+		tempBlock.picturePath = elements[4];
+		
+		//add block to hash for all associated fiducials
+		for(int x = 0; x < associatedFiducials.length; ++x)
+		{
+			blocks.put(new Integer(associatedFiducials[x]), tempBlock);
+		}
 	}
 	
+	/*
 	private boolean saveSmartBlockData(String fileName)
 	{
-		return false;
+		return true;
 	}
 	
 	private boolean saveCommandBlockData(String fileName)
 	{
-		return false;
+		return true;
 	}
 	
 	private boolean saveUserBlockData(String fileName)
 	{
-		return false;
+		return true;
 	}
+	*/
 	
 	public void insertBlock(Block block)
 	{
@@ -81,10 +296,6 @@ class BlockDatabase
 	
 	public boolean saveDatabase()
 	{
-		saveSmartBlockData(smartBlockPath);
-		saveCommandBlockData(commandBlockPath);
-		saveUserBlockData(userBlockPath);
-		
 		return false;
 	}
 }
