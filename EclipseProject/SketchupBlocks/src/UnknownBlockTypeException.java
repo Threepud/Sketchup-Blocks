c4a04ea59cb99eef25a0725e6ff7fbae0c9c1bc9
Jacques Coetzee <jacqueslctz@gmail.com>
2013-06-28 13:39:15 +0200

    Added exception class.
    Exception class added for unknown block type.

---
+++ EclipseProject/SketchupBlocks/src/UnknownBlockTypeException.java
@@ -0,0 +1,9 @@
+public class UnknownBlockTypeException extends RuntimeException 
+{
+	private static final long serialVersionUID = 8341776867784991342L;
+
+	UnknownBlockTypeException(String message)
+	{
+		super(message);
+	}
+}
