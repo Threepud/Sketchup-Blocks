73255e0e6b3b3e1aa96850ea4a6ac26dfd77a589
Jacques Coetzee <jacqueslctz@gmail.com>
2013-06-28 13:38:44 +0200

    Added exception class.
    Exception class added for unknown command block type exception.

---
+++ EclipseProject/SketchupBlocks/src/UnkownCommandBlockTypeException.java
@@ -0,0 +1,9 @@
+public class UnkownCommandBlockTypeException extends RuntimeException 
+{
+	private static final long serialVersionUID = 5238520904156237103L;
+
+	UnkownCommandBlockTypeException(String message)
+	{
+		super(message);
+	}
+}
