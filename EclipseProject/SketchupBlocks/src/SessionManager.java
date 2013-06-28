86d7b6623d295c24914b8a19be6eb5be8e13351b
Elre <vze.ezv@gmail.com>
2013-06-28 13:12:05 +0200

    Edited SessionManager's onCameraEvent
    Implemented the onCameraEvent so that it will distribute the event to
    the appropriate classes.
    Also implemented the creation of the menu and the database.
    Also added the saveProject() method that is required for the user to
    save projects.

--- EclipseProject/SketchupBlocks/src/SessionManager.java
+++ EclipseProject/SketchupBlocks/src/SessionManager.java
@@ -7,17 +7,33 @@ class SessionManager
 	private Exporter kreshnik;
 	private ModelViewer sarah;
 	private BlockDatabase blockDB;
+	private Menu menu;
 	
 	public SessionManager(PApplet _parent)
 	{
 		parent = _parent;
+    	blockDB = new BlockDatabase();
+    	menu = new Menu(this);
 	}
 	
     public void onCameraEvent(CameraEvent cameraEvent)
     {
     	parent.println("Received camera event");
     	//We still require some sort of menu traversal scheme...
-    	
+    	Block block = blockDB.findBlock(cameraEvent.fiducialID);
+    	if(block instanceof UserBlock)
+    	{
+    		System.out.println("This feature is not yet supported");
+    	}
+    	else if (block instanceof CommandBlock)
+    	{
+    		menu.handleInput((CommandBlock)block);
+    	}
+    	else if (block instanceof SmartBlock)
+    	{
+    		ConstructionBlock cblock = new ConstructionBlock((SmartBlock)block, cameraEvent);
+    		jimmy.receiveBlock(cblock);
+    	}
     }
     
     public void setModelConstructor(ModelConstructor _jimmy)
@@ -48,6 +64,10 @@ class SessionManager
     public void exportToFile()
     {
       
+    }
+    
+    public void saveProject()
+    {
     }
     
     public void spectate()
