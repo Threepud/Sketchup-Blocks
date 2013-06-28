17d963a3c865715a5d9deaf7489a0920120540d5
Elre <vze.ezv@gmail.com>
2013-06-28 13:21:28 +0200

    Reattempting recommit of added enum to CommandBlock

--- EclipseProject/SketchupBlocks/src/CommandBlock.java
+++ EclipseProject/SketchupBlocks/src/CommandBlock.java
@@ -1,21 +1,8 @@
 class CommandBlock extends Block
 {
-<<<<<<< HEAD
     public COMMAND_TYPE type;
     public enum COMMAND_TYPE
     {
     	LOAD, CREATE, SAVE, EXPORT, ROTATE, OK, CANCEL
     }
-=======
-	public enum CommandType
-	{
-		NEW,
-		SAVE,
-		LOAD,
-		EXPORT,
-		SPECTATE
-	}
-	
-    public CommandType type;
->>>>>>> 95fcb2d6f35b63578a680b61d7b1f96790fefe4d
 }
