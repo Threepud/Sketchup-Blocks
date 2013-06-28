19ac6d120dc48f05fd9cf0bf42cc28d8ba1dbeea
Elre <vze.ezv@gmail.com>
2013-06-28 13:13:27 +0200

    Added ConstructionBlock constructor
    Added a nice and useful constructor that takes a smartBlock and
    cameraEvent as parameters (since that is what a constructionBlock
    wraps).

--- EclipseProject/SketchupBlocks/src/ConstructionBlock.java
+++ EclipseProject/SketchupBlocks/src/ConstructionBlock.java
@@ -2,4 +2,11 @@ class ConstructionBlock
 {
     public SmartBlock smartBlock;
     public CameraEvent cameraEvent;
+    
+    ConstructionBlock(SmartBlock sblock, CameraEvent cevent)
+    {
+    	smartBlock = sblock;
+    	cameraEvent = cevent;
+    }
+    
 }
