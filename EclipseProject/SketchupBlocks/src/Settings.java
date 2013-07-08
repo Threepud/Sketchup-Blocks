f2ce29ee488ee6d2ac03a80f3bdac7decb25f957
Elre <vze.ezv@gmail.com>
2013-07-08 15:12:38 +0200

    Added Settings class
    Added a static class that stores all the settings. How exactly this is
    populated is yet to be determined.

---
+++ EclipseProject/SketchupBlocks/src/Settings.java
@@ -0,0 +1,9 @@
+
+public class Settings 
+{
+	public static String slotDirectory = "slots";
+	public static int numSlots = 5;
+	
+	public static int numCameras = 1;
+	public static int[] cameraPorts = {3333};
+}
