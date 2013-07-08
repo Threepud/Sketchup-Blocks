ed84b00b956ffec7f5107cbcf034ed6c69e48dd3
Elre <vze.ezv@gmail.com>
2013-07-08 16:34:55 +0200

    Added output
    Added general menu output as well as output for high verbose levels.
    Also fixed the handleInput function header and the loadProject call to
    sessionManager.

--- EclipseProject/SketchupBlocks/src/LoadingState.java
+++ EclipseProject/SketchupBlocks/src/LoadingState.java
@@ -8,15 +8,26 @@ public class LoadingState extends MenuState
 	{
 		super(_sessMan);
 		childState = new SlotChooserState(_sessMan);
+		System.out.println("Please select a slot to load a project from");
 	}
 	
-	public boolean handleInput(CommandBlock cBlock)
+	public boolean handleInput(CommandBlock cBlock, CameraEvent cEvent)
 	{
-		boolean childDone = childState.handleInput(cBlock);
-		if (childDone)
+		if (Settings.verbose >= 3)
+			System.out.println("--Load state is handling input--");
+		boolean childDone = childState.handleInput(cBlock, cEvent);
+		if (childDone && childState.slotNumber != -1) 
 		{
-			sessMan.loadProject();
+			sessMan.loadProject(childState.slotNumber);
+			if (Settings.verbose >= 1)
+				System.out.println("Loading of project was confirmed to slot: "+childState.slotNumber);
 		}
+		
+		if (childState.slotNumber == -1 && Settings.verbose >= 1)
+		{
+			System.out.println("Loading of project was cancelled");
+		}
+		
 		return childDone;
 	}
 }
