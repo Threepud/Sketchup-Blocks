96d0b46083a287bec0f59c24f19b01a834dd0fd4
Elre <vze.ezv@gmail.com>
2013-07-08 16:30:20 +0200

    Added scrollTrigger, slotName and verbose level
    Added a scrollTrigger which controls the number of degrees of rotation
    of the scroll command block that will trigger a slot change.
    Added a slotName, which is the default name of the slot files (with the
    appropriate slot number appended).
    Added a verbose level. 0 is the user default, 3 is the current maximum.
    Higher means more verbose.

--- EclipseProject/SketchupBlocks/src/Settings.java
+++ EclipseProject/SketchupBlocks/src/Settings.java
@@ -2,8 +2,12 @@
 public class Settings 
 {
 	public static String slotDirectory = "slots";
+	public static String slotName = "Slot"; //Append a number to the end of this.
 	public static int numSlots = 5;
+	public static int scrollTrigger = 15;
 	
 	public static int numCameras = 1;
 	public static int[] cameraPorts = {3333};
+	
+	public static int verbose = 3; //0 is default. From there, higher is more verbose. 
 }
