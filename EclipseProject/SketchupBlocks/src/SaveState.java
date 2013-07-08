59769543cd2437347b86cafb550f414a56bc1934
Elre <vze.ezv@gmail.com>
2013-07-08 16:35:33 +0200

    Added output
    Added general menu output as well as output for high verbose levels.
    Also fixed the handleInput function header and the saveProject call to
    sessionManager.

--- EclipseProject/SketchupBlocks/src/SaveState.java
+++ EclipseProject/SketchupBlocks/src/SaveState.java
@@ -6,15 +6,24 @@ public class SaveState extends MenuState
 	{
 		super(_sessMan);
 		childState = new SlotChooserState(_sessMan);
+		System.out.println("Please select a slot to save the project into");
 	}
 	
-	public boolean handleInput(CommandBlock cBlock)
+	public boolean handleInput(CommandBlock cBlock, CameraEvent cEvent)
 	{
-		boolean childDone = childState.handleInput(cBlock);
-		if (childDone)
+		boolean childDone = childState.handleInput(cBlock, cEvent);
+		if (childDone && childState.slotNumber != -1)
 		{
-			sessMan.saveProject();
+			sessMan.saveProject(childState.slotNumber);
+			if (Settings.verbose >= 1)
+				System.out.println("Saving of project was confirmed to slot: "+childState.slotNumber);
 		}
+		
+		if (childState.slotNumber == -1 && Settings.verbose >= 1)
+		{
+			System.out.println("Saving of project was cancelled");
+		}
+		
 		return childDone;
 	}
 }
