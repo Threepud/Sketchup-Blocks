a84b5e0a9267a0a0097c686c7bee9ecdf33ad7c1
Jacques Coetzee <jacqueslctz@gmail.com>
2013-07-15 17:42:04 +0200

    Fixed indentation.

--- EclipseProject/SketchupBlocks/src/LocalLobby.java
+++ EclipseProject/SketchupBlocks/src/LocalLobby.java
@@ -2,31 +2,31 @@ import java.util.ArrayList;
 
 class LocalLobby implements Lobby
 {
-  private Model model;
-  private ArrayList<ModelChangeListener> modelChangeListeners = new ArrayList<ModelChangeListener>();
-  
-    public void updateModel(ModelBlock modelBlock)
-    {
-       model.addModelBlock(modelBlock);
-       modelChangeListeners.trimToSize();
-       for (int k = 0; k < modelChangeListeners.size(); k++)
-      {
-        modelChangeListeners.get(k).fireModelChangeEvent(modelBlock);
-      }
-    }
-    
-    public Model getModel()
-    {
-      return model;
-    }
-    
-    public void setModel(Model _model)
-    {
-      model = _model;
-    }
-    
-    public void registerChangeListener(ModelChangeListener _listener)
-    {
-      modelChangeListeners.add(_listener);
-    }
+	private Model model;
+	private ArrayList<ModelChangeListener> modelChangeListeners = new ArrayList<ModelChangeListener>();
+	  
+	public void updateModel(ModelBlock modelBlock)
+	{
+		model.addModelBlock(modelBlock);
+		modelChangeListeners.trimToSize();
+		for (int k = 0; k < modelChangeListeners.size(); k++)
+		{
+			modelChangeListeners.get(k).fireModelChangeEvent(modelBlock);
+		}
+	}
+	    
+	public Model getModel()
+	{
+		return model;
+	}
+	    
+	public void setModel(Model _model)
+	{
+		model = _model;
+	}
+	    
+	public void registerChangeListener(ModelChangeListener _listener)
+	{
+		modelChangeListeners.add(_listener);
+	}
 }
