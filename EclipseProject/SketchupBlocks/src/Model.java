b16724365d33c2697c1b65a96d9bb557adc1e396
Jacques Coetzee <jacqueslctz@gmail.com>
2013-07-15 17:45:12 +0200

    Fixed indentation.

--- EclipseProject/SketchupBlocks/src/Model.java
+++ EclipseProject/SketchupBlocks/src/Model.java
@@ -3,22 +3,22 @@ import java.util.ArrayList;
 
 class Model
 {
-  private String id;
-  private ArrayList<ModelBlock> blocks;
-  public static int classIdCounter = 0 ;
-  
-  Model()
-  {
-    id = new Integer(classIdCounter++).toString();
-  }
-  
-  void addModelBlock(ModelBlock modelBlock)
-  {
-    blocks.add(modelBlock);
-  }
-  
-  public String getId()
-  {
-   return id; 
-  }
+	private String id;
+	private ArrayList<ModelBlock> blocks;
+	public static int classIdCounter = 0 ;
+	  
+	Model()
+	{
+		id = new Integer(classIdCounter++).toString();
+	}
+	  
+	void addModelBlock(ModelBlock modelBlock)
+	{
+		blocks.add(modelBlock);
+	}
+	  
+	public String getId()
+	{
+		return id; 
+	}
 }
