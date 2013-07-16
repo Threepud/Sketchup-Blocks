d440d54681ddfc33034e777fb03ae6b35cc87c62
Jacques Coetzee <jacqueslctz@gmail.com>
2013-07-16 16:25:45 +0200

    Exception class when given block has no type.
    Created mainly for ModelBlocks but room is left to use for other kinds
    of blocks too.

---
+++ EclipseProject/SketchupBlocks/src/BlockNoTypeException.java
@@ -0,0 +1,10 @@
+
+public class BlockNoTypeException extends RuntimeException
+{
+	private static final long serialVersionUID = -6975572899865810978L;
+
+	BlockNoTypeException(String message)
+	{
+		super(message);
+	}
+}
