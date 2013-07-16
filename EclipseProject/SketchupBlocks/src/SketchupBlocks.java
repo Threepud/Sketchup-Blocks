5aa6150c39023878fa0439fc37dcaf7c8251d15e
Jacques Coetzee <jacqueslctz@gmail.com>
2013-07-16 15:44:25 +0200

    Added the session manager draw call to create GUI.
    Session manager will then pass the draw call to menu and model viewer.

--- EclipseProject/SketchupBlocks/src/SketchupBlocks.java
+++ EclipseProject/SketchupBlocks/src/SketchupBlocks.java
@@ -14,17 +14,17 @@ public class SketchupBlocks extends PApplet
 	
 	public void setup()
 	{
+		size(640, 480, P3D);
 		
 		sessMan = new SessionManager(this);
 		for (int k = 0;  k < Settings.numCameras; k++)
 		{
 			wimpie = new Interpreter(Settings.cameraPorts[k], sessMan, this);
-			
 		}
 	}
 
 	public void draw() 
 	{
-	
+		sessMan.drawGUI();
 	}
 }
