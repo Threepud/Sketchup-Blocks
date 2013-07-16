e3020bfd403e5a3adc5285146c6fb54804b5da0e
Jacques Coetzee <jacqueslctz@gmail.com>
2013-07-16 16:24:45 +0200

    Exception class when expected block isn't found.

---
+++ EclipseProject/SketchupBlocks/src/BlockNotFoundException.java
@@ -0,0 +1,10 @@
+
+public class BlockNotFoundException extends RuntimeException
+{
+	private static final long serialVersionUID = 9206965750508629381L;
+
+	public BlockNotFoundException(String message)
+	{
+		super(message);
+	}
+}
