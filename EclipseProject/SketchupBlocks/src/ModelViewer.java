578acceaa934400717aaa34fdbdd55baf6a7b0d9
Jacques Coetzee <jacqueslctz@gmail.com>
2013-07-16 17:05:26 +0200

    Filled in fireModelChangeEvent to edit cached blocks when needed.

--- EclipseProject/SketchupBlocks/src/ModelViewer.java
+++ EclipseProject/SketchupBlocks/src/ModelViewer.java
@@ -1,9 +1,11 @@
+import java.util.ArrayList;
 import processing.core.PApplet;
 
 class ModelViewer implements ModelChangeListener
 {
 	PApplet window;
 	Lobby lobby;
+	ArrayList<ModelBlock> blockList = null;
 	
 	public ModelViewer()
 	{
@@ -15,14 +17,59 @@ class ModelViewer implements ModelChangeListener
 		window = _window;
 	}
 	
-	public void setLobby(Lobby _lobby)
+	public void setLobby(Lobby _lobby) throws RuntimeException
 	{
 	    lobby = _lobby;
+	    Model model = lobby.getModel();
+	    
+	    if(model == null)
+	    	throw new ModelNotSetException("Model Viewer: Model not set.");
+	    else
+	    	blockList = new ArrayList<>(model.getBlocks());
 	}
 	  
-	public void fireModelChangeEvent(ModelBlock change)
+	public void fireModelChangeEvent(ModelBlock change) throws RuntimeException
 	{
-	    
+		int index;
+		
+	    switch(change.type)
+	    {
+		    case ADD:
+		    	blockList.add(change);
+		    	break;
+		    case UPDATE:
+		    	index = findBlockIndex(change.smartBlock.blockId);
+		    	blockList.set(index, change);
+		    	break;
+		    case DELETE:
+		    	index = findBlockIndex(change.smartBlock.blockId);
+		    	blockList.remove(index);
+		    	break;
+	    	default:
+	    		throw new BlockNoTypeException("Model Viewer: No Model Block Type.");
+	    }
+	}
+	
+	private int findBlockIndex(int blockID)
+	{
+    	int index = -1;
+    	for(ModelBlock tempBlock: blockList)
+    	{
+    		if(tempBlock.smartBlock.blockId == blockID)
+    		{
+    			index++;
+    			break;
+    		}
+    		else
+    		{
+    			index++;
+    		}
+    	}
+    	
+    	if(index == -1 || index == blockList.size())
+    		throw new BlockNotFoundException("Model Viewer: Update block not found.");
+    	
+    	return index;
 	}
 	
 	public void drawModel()
