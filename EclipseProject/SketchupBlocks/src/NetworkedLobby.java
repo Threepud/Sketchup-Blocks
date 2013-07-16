9c12e34259aa533fb8a34a3739e2edcf6871f827
Neoin <heinoldewage@gmail.com>
2013-07-16 14:30:40 +0200

    Will now connect and keep a model
    Still needs to listen for messages from server.

--- EclipseProject/SketchupBlocks/src/NetworkedLobby.java
+++ EclipseProject/SketchupBlocks/src/NetworkedLobby.java
@@ -1,20 +1,56 @@
-class NetworkedLobby implements Lobby
+import java.net.Socket;
+import java.util.ArrayList;
+
+
+class NetworkedLobby extends Thread implements Lobby
 {
+	Socket connection;
+	private Model model;
+	private ArrayList<ModelChangeListener> modelChangeListeners = new ArrayList<ModelChangeListener>();
+	  	
+  public NetworkedLobby(String server, int port)
+  {
+	  try
+	  {
+	  connection = new Socket(server,port);
+	  }
+	  catch(Exception e)
+	  {
+		  e.printStackTrace();
+	  }
+	  
+  }
+	
   public void updateModel(ModelBlock modelBlock)
   {
+		model.addModelBlock(modelBlock);
+		modelChangeListeners.trimToSize();
+		for (int k = 0; k < modelChangeListeners.size(); k++)
+		{
+			modelChangeListeners.get(k).fireModelChangeEvent(modelBlock);
+		}
   }
   
   public Model getModel()
   {
-    return null;
+    return model;
   }
   
   public void setModel(Model model)
   {
+	  
   }
   
   public void registerChangeListener(ModelChangeListener listener)
   {
+	  modelChangeListeners.add(listener);
+  }
+  
+  @Override
+  public void run()
+  {
+	  
+	  
   }
   
 }
