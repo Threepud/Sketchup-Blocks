0a342afef34dd7745bbf3c15e42bd0354a8645b5
Jacques Coetzee <jacqueslctz@gmail.com>
2013-07-16 11:07:28 +0200

    Added ignore annotation.
    Annotation added because the test is not yet complete.

--- EclipseProject/SketchupBlocks/test/BlockDatabaseTest.java
+++ EclipseProject/SketchupBlocks/test/BlockDatabaseTest.java
@@ -1,9 +1,11 @@
 import static org.junit.Assert.*;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class BlockDatabaseTest 
 {
+	@Ignore("Not yet implemented.")
 	@Test
 	public void testFindBlock() 
 	{
