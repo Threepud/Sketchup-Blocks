3794bb7f9f39c98d43f4bd624e24e55121765aad
Jacques Coetzee <jacqueslctz@gmail.com>
2013-07-16 16:24:17 +0200

    Exception class when model reference is null.

---
+++ EclipseProject/SketchupBlocks/src/ModelNotSetException.java
@@ -0,0 +1,10 @@
+
+public class ModelNotSetException extends RuntimeException 
+{
+	private static final long serialVersionUID = 3579876965324893510L;
+
+	public ModelNotSetException(String message)
+	{
+		super(message);
+	}
+}
