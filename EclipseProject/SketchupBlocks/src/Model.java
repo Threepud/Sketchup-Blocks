6b6313c8a3b9fa1012f0114312e52f6989dfb132
Neoin <heinoldewage@gmail.com>
2013-07-16 12:34:38 +0200

    Allowed for block update and access
    The update is done by the hashmap and access done through two getters.
    One getting by Id and the other returning the list.

--- EclipseProject/SketchupBlocks/src/Model.java
+++ EclipseProject/SketchupBlocks/src/Model.java
@@ -1,24 +1,36 @@
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Collection;
 
 
 class Model
 {
 	private String id;
-	private ArrayList<ModelBlock> blocks;
+	private HashMap<Integer,ModelBlock> blockMap;
 	public static int classIdCounter = 0 ;
 	  
 	Model()
 	{
 		id = new Integer(classIdCounter++).toString();
+		blockMap = new HashMap<Integer,ModelBlock>();
 	}
 	  
 	void addModelBlock(ModelBlock modelBlock)
 	{
-		blocks.add(modelBlock);
+		blockMap.put(new Integer(modelBlock.smartBlock.blockId),modelBlock);
 	}
 	  
 	public String getId()
 	{
 		return id; 
+	}
+	
+	public Collection<ModelBlock> getBlocks()
+	{
+		return blockMap.values() ;
+	}
+	
+	public ModelBlock getBlockById(Integer id)
+	{
+		return blockMap.get(id);
 	}
 }
