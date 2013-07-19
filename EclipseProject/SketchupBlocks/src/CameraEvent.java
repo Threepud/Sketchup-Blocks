9a72efa04a40a8f9917e3a2adc721a2110db9756
Elre <vze.ezv@gmail.com>
2013-07-19 11:38:27 +0200

    Moved field
    Moved the cameraID field to the top of the field list to keep Eclipse
    from being sad.

--- EclipseProject/SketchupBlocks/src/CameraEvent.java
+++ EclipseProject/SketchupBlocks/src/CameraEvent.java
@@ -4,11 +4,11 @@ class CameraEvent
 	  {
 		  ADD, REMOVE, UPDATE
 	  }
-	
+
+	public int cameraID;
 	public float x;
 	public float y;
 	public float rotation;
 	public EVENT_TYPE type;
 	public int fiducialID;
-	public int cameraID;
 }
