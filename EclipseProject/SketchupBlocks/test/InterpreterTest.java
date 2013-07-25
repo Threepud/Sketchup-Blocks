b7d2fcf06efb0445535ab6aa0cf4547e4153cf5e
Elre <vze.ezv@gmail.com>
2013-07-25 17:22:36 +0200

    Created Test classes
    Created test classes for testing the Menu, MenuStates, LinearSolver and
    Interpreter.

---
+++ EclipseProject/SketchupBlocks/test/InterpreterTest.java
@@ -0,0 +1,32 @@
+
+import static org.junit.Assert.*;
+import org.junit.*;
+import org.junit.rules.ExpectedException;
+import org.junit.runners.*;
+import org.junit.runner.RunWith;
+
+/*
+Run with:
+java -cp .;junit.jar;hamcrest-core-1.3.jar org.junit.runner.JUnitCore Matrix
+
+*/
+
+@RunWith(JUnit4.class)
+public class InterpreterTest
+{
+	
+	@Before
+	public void setup()
+	{
+		
+	}
+	
+	
+	
+	@Test
+	public void testConstructor()
+	{
+		
+	}
+
+} 
