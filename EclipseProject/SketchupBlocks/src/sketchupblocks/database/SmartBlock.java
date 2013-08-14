6df9ffa9ea0d88f8758a947230e1698666ca6474
Elre <vze.ezv@gmail.com>
2013-08-14 11:55:26 +0200

    Removed fiducialCoordinates attribute from SmartBlock. Moved it to Block.

--- EclipseProject/SketchupBlocks/src/sketchupblocks/database/SmartBlock.java
+++ EclipseProject/SketchupBlocks/src/sketchupblocks/database/SmartBlock.java
@@ -7,7 +7,6 @@ public class SmartBlock extends Block
 	public String name;
 	public Vec3[] vertices;
 	public int[] indices;
-	public Vec3[] fiducialCoordinates;
 
 	public boolean areOppositeFidicials(int fiducialID, int fiducialID2) 
 	{
