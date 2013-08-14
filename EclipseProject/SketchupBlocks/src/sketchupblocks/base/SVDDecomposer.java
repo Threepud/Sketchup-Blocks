2da83f1c277fc238e5c9a2a774f20958d8f06f38
Elre <vze.ezv@gmail.com>
2013-08-14 11:53:50 +0200

    Created placeholder class
    TO BE IMPLEMENTED. Necessary for MysticalModelCenterCalculator.

---
+++ EclipseProject/SketchupBlocks/src/sketchupblocks/base/SVDDecomposer.java
@@ -0,0 +1,12 @@
+package sketchupblocks.base;
+
+import sketchupblocks.math.Matrix;
+import sketchupblocks.math.Vec3;
+
+public class SVDDecomposer 
+{
+	public static Matrix getRotationMatrix(Vec3[] inputData, Vec3[] movedData)
+	{
+		return null;
+	}
+}
