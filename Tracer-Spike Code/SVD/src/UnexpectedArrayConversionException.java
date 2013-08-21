a77c92e654fb52ddc9969895fafd4d61598b0d4a
Elre <vze.ezv@gmail.com>
2013-08-22 00:05:39 +0200

    Created new exception for matrix class

---
+++ Tracer-Spike Code/SVD/src/UnexpectedArrayConversionException.java
@@ -0,0 +1,10 @@
+
+public class UnexpectedArrayConversionException extends Exception
+{
+	private static final long serialVersionUID = 1L;
+
+	public UnexpectedArrayConversionException(String message)
+	{
+		super(message);
+	}
+}
