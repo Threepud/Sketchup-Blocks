0c936fcc8362d4ff2122dd851203e47622bbdea0
Elre <vze.ezv@gmail.com>
2013-08-22 23:11:07 +0200

    Added getOffset function and make provision for 4x4 transformations

--- EclipseProject/SketchupBlocks/src/sketchupblocks/base/ModelCenterCalculator.java
+++ EclipseProject/SketchupBlocks/src/sketchupblocks/base/ModelCenterCalculator.java
@@ -5,6 +5,7 @@ import sketchupblocks.math.Line;
 import sketchupblocks.math.LinearSystemSolver;
 import sketchupblocks.math.Matrix;
 import sketchupblocks.math.RotationMatrix3D;
+import sketchupblocks.math.RotationMatrix4D;
 import sketchupblocks.math.Vec3;
 import sketchupblocks.math.Vec4;
 
@@ -30,27 +31,26 @@ public class ModelCenterCalculator
 				//Now translate these points so that m1m2Mid is at the origin.
 				Vec3 origin = new Vec3(0, 0, 0);
 				Matrix mTranslation = getTranslationMatrix(m1m2Mid, origin);
-				Vec3 m1o = Matrix.multiply(mTranslation, new Vec4(m1)).toVec3();
-				Vec3 m2o = Matrix.multiply(mTranslation, new Vec4(m2)).toVec3();
+				Vec3 m1o = Matrix.multiply(mTranslation, m1.padVec3()).toVec3();
+				Vec3 m2o = Matrix.multiply(mTranslation, m2.padVec3()).toVec3();
 				
 				//Translate the fiducials' midpoint to the origin.
 				//Multiply the fiducial positions by the same translation matrix.
 				Vec3 w1w2Mid = Vec3.midpoint(positions[0], positions[1]);
 				Matrix wTranslation = getTranslationMatrix(w1w2Mid, origin);
-				Vec3 w1o = Matrix.multiply(wTranslation, new Vec4(positions[0])).toVec3();
-				Vec3 w2o = (Matrix.multiply(wTranslation, new Vec4(positions[1]))).toVec3();
+				Vec3 w1o = Matrix.multiply(wTranslation, positions[0].padVec3()).toVec3();
+				Vec3 w2o = (Matrix.multiply(wTranslation, positions[1].padVec3())).toVec3();
 				
 				
 				Vec3 dm = Vec3.subtract(m1, m2);
 				Vec3 d = Vec3.subtract(positions[0], positions[1]);
-				
+
 				//Choose offset in some direction perpendicular to the d.
-				Vec3 offset = new Vec3(1, 1, -(d.x + d.y)/d.z);
-				offset.normalize();
+				Vec3[] offsets = getOffsets(dm, d);
+				Vec3 oOffset = offsets[0];
+				Vec3 offset = offsets[1];
 				Vec3 offsetPoint = Vec3.add(origin, offset);
-				//Choose a similar offset,  perpendicular to dm.
-				Vec3 oOffset = new Vec3(1, 1, -(dm.x + dm.y)/dm.z);
-				Vec3 oOffsetPoint = Vec3.add(origin, Vec3.normalize(oOffset));
+				Vec3 oOffsetPoint = Vec3.add(origin, oOffset);
 				
 				//Now we have 3 points, so we can continue from there....
 				//Get the rotation between the model points and fiducial points
@@ -64,14 +64,14 @@ public class ModelCenterCalculator
 				//So now we can rotate about x and y
 				Matrix cobDSpace = new Matrix(new Vec3[]{oOffset, basis2, dm}, true);
 				
-				Matrix toDWorld = Matrix.multiply(Matrix.multiply(cobDSpace, R), wTranslation);
+				Matrix toDWorld = Matrix.multiply(Matrix.multiply(cobDSpace, R).padMatrix(), wTranslation);
 				
-				Vec3[] dFidUp = new Vec3[2];
+				Vec4[] dFidUp = new Vec4[2];
 				for (int k = 0; k < 2; k++)
 				{
 					//Transform our fiducial normals from model space to D world.
-					dFidUp[k] = sBlock.fiducialOrient[fidIDs[k]];
-					dFidUp[k] = Matrix.multiply(mTranslation, new Vec4(dFidUp[k])).toVec3();
+					dFidUp[k] = new Vec4(sBlock.fiducialOrient[fidIDs[k]]);
+					dFidUp[k] = Matrix.multiply(mTranslation, dFidUp[k]);
 					dFidUp[k] = Matrix.multiply(toDWorld, dFidUp[k]);
 				}
 				
@@ -79,8 +79,8 @@ public class ModelCenterCalculator
 				for (int i = 0; i < 2; i++)
 					rotation[i] = Matrix.multiply(toDWorld, new Vec4(rotation[i])).toVec3();
 				
-				RotationMatrix3D rTry = new RotationMatrix3D(0);
-				Vec3[] tryUps = new Vec3[2];
+				RotationMatrix4D rTry = new RotationMatrix4D(0);
+				Vec4[] tryUps = new Vec4[2];
 				double [] scores = new double[360];
 				//Now we rotate them iteratively
 				for (int k = 0; k < 360; k += 2)
@@ -94,7 +94,7 @@ public class ModelCenterCalculator
 					{
 						tryUps[i] = Matrix.multiply(rTry, dFidUp[i]);
 						
-						Vec3 uhm1 = getProjection(oOffset,basis2,tryUps[i]);
+						Vec3 uhm1 = getProjection(oOffset,basis2,tryUps[i].toVec3());
 						Vec3 uhm2 = getProjection(oOffset,basis2,rotation[i]);
 						
 						error += Math.acos(Vec3.dot(uhm1, uhm2));
@@ -110,7 +110,7 @@ public class ModelCenterCalculator
 				
 				//so highest is the best rotation
 				rTry.updateTheta(highest);
-				Matrix modelToDSpace = Matrix.multiply(cobDSpace,mTranslation);
+				Matrix modelToDSpace = Matrix.multiply(cobDSpace.padMatrix(),mTranslation);
 				Matrix DSpaceToWorld = Matrix.multiply(toDWorld.getInverse(),rTry);
 				Matrix modelToWorld = Matrix.multiply(DSpaceToWorld,modelToDSpace);
 				
@@ -124,14 +124,39 @@ public class ModelCenterCalculator
 		return null;
 	}
 	
-	static Vec3 getProjection(Vec3 k1,Vec3 k2, Vec3 point)
+	private static Vec3[] getOffsets(Vec3 dm, Vec3 d) throws Exception
+	{
+		Vec3[] result = new Vec3[2];
+		int index = 0;
+		if (dm.x != 0 && d.x != 0)
+		{
+			result[index++] = Vec3.normalize(new Vec3(-(dm.y + dm.z)/dm.x, 1, 1));
+			result[index++] = Vec3.normalize(new Vec3(-(d.y + d.z)/d.x, 1, 1));
+		}
+		else if (dm.y != 0 && d.y != 0)
+		{
+			result[index++] = Vec3.normalize(new Vec3(1, -(dm.x + dm.z)/dm.y, 1));
+			result[index++] = Vec3.normalize(new Vec3(1, -(d.x + d.z)/d.y, 1));
+		}
+		else if (dm.z != 0 && d.z != 0)
+		{
+			result[index++] = Vec3.normalize(new Vec3(1, 1, -(dm.x + dm.y)/dm.z));
+			result[index++] = Vec3.normalize(new Vec3(1, 1, -(d.x + d.y)/d.z));
+		}
+		else
+			throw new Exception("Now offset can be calculated for given d's");
+		return result;
+	}
+	
+	
+	private static Vec3 getProjection(Vec3 k1,Vec3 k2, Vec3 point)
 	{
 		double scale1 =Vec3.dot(Vec3.normalize(k1),Vec3.normalize(point));
 		double scale2 =Vec3.dot(Vec3.normalize(k2),Vec3.normalize(point));
 		return Vec3.add(Vec3.scalar(scale1,k1), Vec3.scalar(scale2,k2));		
 	}
 	
-	static Matrix getTranslationMatrix(Vec3 one, Vec3 two)
+	private static Matrix getTranslationMatrix(Vec3 one, Vec3 two)
 	{
 		Matrix res = new Matrix(4, 4);
 		double[][] data = new double[4][];
