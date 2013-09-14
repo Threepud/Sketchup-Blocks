61d13a3520fd58eb8b2dc14c6eda5880477f0374
Jacques Coetzee <jacqueslctz@gmail.com>
2013-09-14 22:02:59 +0200

    Added additional code to support networking (spectation).

--- EclipseProject/SketchupBlocks/src/sketchupblocks/network/NetworkedLobby.java
+++ EclipseProject/SketchupBlocks/src/sketchupblocks/network/NetworkedLobby.java
@@ -10,83 +10,79 @@ import sketchupblocks.base.ModelChangeListener;
 import sketchupblocks.exception.ModelNotSetException;
 
 
-class NetworkedLobby extends Thread implements Lobby
+public class NetworkedLobby extends Thread implements Lobby
 {
 	private Socket connection;
 	private Model model;
 	private ArrayList<ModelChangeListener> modelChangeListeners = new ArrayList<ModelChangeListener>();
-	  	
-  public NetworkedLobby(String server, int port)
-  {
-	  try
-	  {
-	  connection = new Socket(server,port);
-	  }
-	  catch(Exception e)
-	  {
-		  e.printStackTrace();
-	  }
-	  
-  }
-	
-  public void updateModel(ModelBlock modelBlock)
-  {
+	private boolean online = true;
+
+	public NetworkedLobby(String server, int port)
+	{
+		try
+		{
+			connection = new Socket(server,port);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+
+	}
+
+	public void updateModel(ModelBlock modelBlock)
+	{
 		model.addModelBlock(modelBlock);
 		modelChangeListeners.trimToSize();
 		for (int k = 0; k < modelChangeListeners.size(); k++)
 		{
-			try 
-			{
-				modelChangeListeners.get(k).fireModelChangeEvent(modelBlock);
-			} 
-			catch (Exception e) 
-			{
-				e.printStackTrace();
-			}
+			modelChangeListeners.get(k).fireModelChangeEvent(modelBlock);
 		}
-  }
-  
-  	public Model getModel() throws ModelNotSetException
+	}
+
+	public Model getModel() throws ModelNotSetException
 	{
 		if(model == null)
 			throw new ModelNotSetException("Local Lobby: Model not set.");
 		return model;
 	}
-  
-  public void setModel(Model model)
-  {
-	  
-  }
-  
-  public void registerChangeListener(ModelChangeListener listener)
-  {
-	  modelChangeListeners.add(listener);
-  }
-  
-  @Override
-  public void run()
-  {
-	 while(true)
-	 {
-		 try
-		 {
-		 ObjectInputStream inp = new ObjectInputStream(connection.getInputStream());
-		 ModelBlock modelBlock = (ModelBlock) inp.readObject();
-		 	model.addModelBlock(modelBlock);
-		 	
-			modelChangeListeners.trimToSize();
-			for (int k = 0; k < modelChangeListeners.size(); k++)
+
+	public void setModel(Model model)
+	{
+
+	}
+
+	public void registerChangeListener(ModelChangeListener listener)
+	{
+		modelChangeListeners.add(listener);
+	}
+
+	@Override
+	public void run()
+	{
+		while(online)
+		{
+			try
 			{
-				modelChangeListeners.get(k).fireModelChangeEvent(modelBlock);
+				ObjectInputStream inp = new ObjectInputStream(connection.getInputStream());
+				ModelBlock modelBlock = (ModelBlock) inp.readObject();
+				model.addModelBlock(modelBlock);
+
+				modelChangeListeners.trimToSize();
+				for (int k = 0; k < modelChangeListeners.size(); k++)
+				{
+					modelChangeListeners.get(k).fireModelChangeEvent(modelBlock);
+				}
 			}
-		 }
-		 catch(Exception e)
-		 {
-			 
-		 }
-		 
-	 }
-	  
-  }
-  
+			catch(Exception e)
+			{
+				System.out.println(e);
+			}
+		}
+	}
+	
+	public void stopLobby()
+	{
+		online = false;
+	}
 }
