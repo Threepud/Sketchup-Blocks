7251975a172f2e4c5c5b7d80f961cc21eb33dd42
Elre <vze.ezv@gmail.com>
2013-09-24 17:05:21 +0200

    Created BoundingBox class (again).

--- EclipseProject/SketchupBlocks/src/sketchupblocks/construction/BoundingBox.java
+++ EclipseProject/SketchupBlocks/src/sketchupblocks/construction/BoundingBox.java
@@ -1,5 +1,6 @@
 package sketchupblocks.construction;
 
+import sketchupblocks.math.Matrix;
 import sketchupblocks.math.Vec3;
 
 public class BoundingBox 
@@ -11,5 +12,26 @@ public class BoundingBox
 	{
 		min = _min;
 		max = _max;
+	}
+	
+	public static BoundingBox generateBoundingBox(ModelBlock mb)
+	{
+		double[] max = new double[]{Double.MIN_VALUE, Double.MIN_VALUE, Double.MIN_VALUE};
+		double[] min = new double[]{Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE};
+		
+		Vec3[] vertices = Matrix.multiply(mb.transformationMatrix, new Matrix(mb.smartBlock.vertices, true)).toVec3Array();
+		for (int k = 0; k < vertices.length; k++)
+		{
+			double[] vert = vertices[k].toArray();
+			for (int i = 0; i < vert.length; i++)
+			{
+				if (vert[i] < min[i])
+					min[i] = vert[i];
+				if (vert[i] > max[i])
+					max[i] = vert[i];
+			}
+			
+		}
+		return new BoundingBox(new Vec3(max), new Vec3(min));
 	}
 }
--- EclipseProject/SketchupBlocks/src/sketchupblocks/construction/EnvironmentAnalyzer.java
+++ EclipseProject/SketchupBlocks/src/sketchupblocks/construction/EnvironmentAnalyzer.java
@@ -1,9 +1,6 @@
 package sketchupblocks.construction;
 
 import java.util.Collection;
-
-import sketchupblocks.math.Matrix;
-import sketchupblocks.math.Vec3;
 import sketchupblocks.network.Lobby;
 
 public class EnvironmentAnalyzer 
@@ -19,11 +16,11 @@ public class EnvironmentAnalyzer
 	{
 		try
 		{
-			BoundingBox newBB = generateBoundingBox(newBlock);
+			BoundingBox newBB = BoundingBox.generateBoundingBox(newBlock);
 			Collection<ModelBlock> blocks = eddy.getModel().getBlocks();
 			for (ModelBlock modelBlock : blocks)
 			{
-				BoundingBox modelBB = generateBoundingBox(newBlock);
+				BoundingBox modelBB = BoundingBox.generateBoundingBox(newBlock);
 				
 			}
 		}
@@ -45,26 +42,5 @@ public class EnvironmentAnalyzer
 	{
 
 		return null;
-	}
-	
-	private static BoundingBox generateBoundingBox(ModelBlock mb)
-	{
-		double[] max = new double[]{Double.MIN_VALUE, Double.MIN_VALUE, Double.MIN_VALUE};
-		double[] min = new double[]{Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE};
-		
-		Vec3[] vertices = Matrix.multiply(mb.transformationMatrix, new Matrix(mb.smartBlock.vertices, true)).toVec3Array();
-		for (int k = 0; k < vertices.length; k++)
-		{
-			double[] vert = vertices[k].toArray();
-			for (int i = 0; i < vert.length; i++)
-			{
-				if (vert[i] < min[i])
-					min[i] = vert[i];
-				if (vert[i] > max[i])
-					max[i] = vert[i];
-			}
-			
-		}
-		return new BoundingBox(new Vec3(max), new Vec3(min));
 	}
 }
