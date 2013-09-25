b9315099355baab01a771e9bcdde6f30e7954d70
Elre <vze.ezv@gmail.com>
2013-09-25 13:52:51 +0200

    Removed obsolete function calls

--- EclipseProject/SketchupBlocks/src/sketchupblocks/construction/ModelConstructor.java
+++ EclipseProject/SketchupBlocks/src/sketchupblocks/construction/ModelConstructor.java
@@ -314,9 +314,9 @@ public class ModelConstructor implements Runnable
 			
 			bin.transform = transform;
 			bin.removed = false;
-			//eddy.updateModel((new ModelBlock(sBlock, transform, ModelBlock.ChangeType.UPDATE)));
-			eddy.updateModel(PseudoPhysicsApplicator.applyPseudoPhysics(new ModelBlock(sBlock, transform, ModelBlock.ChangeType.UPDATE)));
-			//eddy.updateModel(new ModelBlock(sBlock, transform, ModelBlock.ChangeType.UPDATE));
+			
+			eddy.updateModel((new ModelBlock(sBlock, transform, ModelBlock.ChangeType.UPDATE)));
+			//eddy.updateModel(PseudoPhysicsApplicator.applyPseudoPhysics(new ModelBlock(sBlock, transform, ModelBlock.ChangeType.UPDATE)));
 		}
 		else
 		{
