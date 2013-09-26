473c7ed9818c724dfab1235e311a09e50cf71c6d
Elre <vze.ezv@gmail.com>
2013-09-26 13:36:05 +0200

    Fixed crossproduct again

--- EclipseProject/SketchupBlocks/src/sketchupblocks/math/Face.java
+++ EclipseProject/SketchupBlocks/src/sketchupblocks/math/Face.java
@@ -21,8 +21,8 @@ public class Face
 		if (corners.length < 3)
 			throw new RuntimeException("Invalid face specification");
 		
-		Vec3 oneToTwo = Vec3.subtract(corners[0], corners[1]);
-		Vec3 oneToThree = Vec3.subtract(corners[0], corners[2]);
+		Vec3 oneToTwo = Vec3.subtract(corners[1], corners[0]);
+		Vec3 oneToThree = Vec3.subtract(corners[2], corners[0]);
 		return Vec3.normalize(Vec3.cross(oneToThree, oneToTwo));
 	}
 }
