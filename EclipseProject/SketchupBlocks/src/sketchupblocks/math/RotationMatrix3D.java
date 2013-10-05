33c906e9f8fccffa1f0b211f1382308a6956d0d0
Hein Oldewage <heinoldewage@gmail.com>
2013-10-05 13:45:01 +0200

    Added constructors for the rotation about all the axis.

--- EclipseProject/SketchupBlocks/src/sketchupblocks/math/RotationMatrix3D.java
+++ EclipseProject/SketchupBlocks/src/sketchupblocks/math/RotationMatrix3D.java
@@ -36,6 +36,34 @@ public class RotationMatrix3D extends Matrix
         this.data = d;
     }
 
+    public RotationMatrix3D(double x, double y, double z)
+    {
+        super(3, 3);
+        axis = Axis.X_AXIS;
+        
+		double[][] dx = new double[3][];
+		dx[0] = new double[]{1, 0, 0};
+		dx[1] = new double[]{0, Math.cos(x), Math.sin(x)};
+	    dx[2] = new double[]{0, -1*Math.sin(x), Math.cos(x)};
+	    Matrix mx = new Matrix(dx);  			
+
+	    double[][] dy = new double[3][];
+		dy[0] = new double[]{Math.cos(y), 0, -1*Math.sin(y)};
+		dy[1] = new double[]{0, 1, 0};
+        dy[2] = new double[]{Math.sin(y), 0, Math.cos(y)};
+        Matrix my = new Matrix(dy);
+        
+        double[][] dz = new double[3][];
+		dz[0] = new double[]{Math.cos(z), Math.sin(z), 0};
+        dz[1] = new double[]{-1*Math.sin(z), Math.cos(z), 0};
+        dz[2] = new double[]{0, 0, 1};
+        Matrix mz = new Matrix(dy); 
+        
+	
+        
+        this.data = Matrix.multiply(mz, Matrix.multiply(my, mx)).data;
+    }   
+    
     public void updateTheta(double theta)
     {
     	switch(axis)
--- EclipseProject/SketchupBlocks/src/sketchupblocks/math/RotationMatrix4D.java
+++ EclipseProject/SketchupBlocks/src/sketchupblocks/math/RotationMatrix4D.java
@@ -1,5 +1,7 @@
 package sketchupblocks.math;
 
+import sketchupblocks.math.Matrix.Axis;
+
 public class RotationMatrix4D extends Matrix 
 {
 	private static final long serialVersionUID = 1L;
@@ -36,6 +38,37 @@ public class RotationMatrix4D extends Matrix
         }
         d[3] = new double[]{0, 0, 0, 1};
         this.data = d;
+    }
+	
+	public RotationMatrix4D(double x, double y, double z)
+    {
+        super(4, 4);
+        axis = Axis.X_AXIS;
+        
+		double[][] dx = new double[4][];
+		dx[0] = new double[]{1, 0, 0, 0};
+		dx[1] = new double[]{0, Math.cos(x), Math.sin(x), 0};
+	    dx[2] = new double[]{0, -1*Math.sin(x), Math.cos(x), 0};
+	    dx[3] = new double[]{0, 0, 0, 1};
+	    Matrix mx = new Matrix(dx);  			
+
+	    double[][] dy = new double[4][];
+		dy[0] = new double[]{Math.cos(y), 0, -1*Math.sin(y), 0};
+		dy[1] = new double[]{0, 1, 0, 0};
+        dy[2] = new double[]{Math.sin(y), 0, Math.cos(y), 0};
+        dy[3] = new double[]{0, 0, 0, 1};
+        Matrix my = new Matrix(dy);
+        
+        double[][] dz = new double[4][];
+		dz[0] = new double[]{Math.cos(z), Math.sin(z), 0, 0};
+        dz[1] = new double[]{-1*Math.sin(z), Math.cos(z), 0, 0};
+        dz[2] = new double[]{0, 0, 1, 0};
+        dz[3] = new double[]{0, 0, 0, 1};
+        Matrix mz = new Matrix(dy); 
+        
+	
+        
+        this.data = Matrix.multiply(mz, Matrix.multiply(my, mx)).data;
     }
 
     public void updateTheta(double theta)
