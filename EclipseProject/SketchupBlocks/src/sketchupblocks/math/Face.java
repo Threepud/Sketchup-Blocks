fb294f215359970897d15355736566d0e9c86d46
Elre <vze.ezv@gmail.com>
2013-10-17 23:22:26 +0200

    Added comments for javadocs.

--- EclipseProject/SketchupBlocks/src/sketchupblocks/math/Face.java
+++ EclipseProject/SketchupBlocks/src/sketchupblocks/math/Face.java
@@ -1,14 +1,35 @@
 package sketchupblocks.math;
 
+/**
+ * @author cravingoxygen
+ *
+ *This class wraps the information that defines a 3D face (in terms of its corners).
+ */
 public class Face
 {
+	/**
+	 * An array of vectors, where each vector is a corner of the Face.
+	 */
 	public Vec3[] corners;
 	
+	
+	/**
+	 * The constructor for a triangular face
+	 * @param c1 - The first corner of the face
+	 * @param c2 - The second corner of the face 
+	 * @param c3 - The third corner of the face
+	 */
 	public Face(Vec3 c1, Vec3 c2, Vec3 c3)
 	{
 		corners = new Vec3[]{c1, c2, c3};
 	}
 	
+	/**
+	 * Constructor for a face that is not necessarily triangular.
+	 * 
+	 * This throws a RuntimeException if the input array isn't at least of size three.
+	 * @param _corners - The array containing the corners that define a face. This must contain at least three entries.
+	 */
 	public Face(Vec3[] _corners)
 	{
 		corners = _corners;
@@ -16,6 +37,16 @@ public class Face
 			throw new RuntimeException("Invalid face specification");
 	}
 	
+	/**
+	 * This calculates and returns the normal of the face.
+	 * A RuntimeException is throws if the face isn't defined by at least three corners.
+	 * 
+	 * a0 = corner1 - corner0;
+	 * a1 = corner2 - corner0;
+	 * normal = a1 cross a0.
+	 * 
+	 * @return - the normal of the face.
+	 */
 	public Vec3 normal()
 	{
 		if (corners.length < 3)
--- EclipseProject/SketchupBlocks/src/sketchupblocks/math/Line.java
+++ EclipseProject/SketchupBlocks/src/sketchupblocks/math/Line.java
@@ -2,13 +2,37 @@ package sketchupblocks.math;
 
 import java.io.Serializable;
 
+/**
+ * The Line class wraps the information contained in a 3D line:
+ * - A point on the line
+ * -The direction of the line
+ * 
+ * @author cravingoxygen
+ *
+ */
 public class Line implements Serializable
 {
 	private static final long serialVersionUID = -1894790789654200614L;
 	
+	/**
+	 * A point on the line.
+	 */
 	public Vec3 point;
+	
+	/**
+	 * The direction of the line.
+	 */
 	public Vec3 direction;
 	
+	/**
+	 * Constructor that accepts a point and direction, thereby defining a line. 
+	 * The line is defined in 3D space.
+	 * 
+	 * point_on_line = lambda*direction + point
+	 * 
+	 * @param _point - The point in the line equation
+	 * @param _direction - The direction vector of the line
+	 */
 	public Line(Vec3 _point, Vec3 _direction)
 	{
 		point = _point;
