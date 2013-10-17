a2560fb6b831f7ba3596f3b423f095d88bad7742
Elre <vze.ezv@gmail.com>
2013-10-17 22:40:20 +0200

    Created test (incomplete) for SessionManager.

---
+++ EclipseProject/SketchupBlocks/test/SessionManagerTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+import processing.core.PApplet;
+import sketchupblocks.base.SessionManager;
+
+
+public class SessionManagerTest 
+{
+
+	@Test
+	public void testConstructor()
+	{
+		PApplet p = new PApplet();
+		SessionManager sess = new SessionManager(p, false);
+	}
+	
+}
