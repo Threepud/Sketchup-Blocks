c6fc411a24c4073c2a7aa086505895cb19ceb44f
Elre <vze.ezv@gmail.com>
2013-10-17 23:22:26 +0200

    Added comments for javadocs.

--- EclipseProject/SketchupBlocks/src/sketchupblocks/math/LinearSystemSolver.java
+++ EclipseProject/SketchupBlocks/src/sketchupblocks/math/LinearSystemSolver.java
@@ -3,14 +3,25 @@ package sketchupblocks.math;
 /**
  *
  * @author cravingoxygen
+ * 
+ * The LinearSystemSolver class solves a given linear system.
+ * The linear system provided must be square and must have a unique solution.
+ * This class makes use of the LU decomposer to solve the system.
  */
 public class LinearSystemSolver 
 {
-
+	
     /**
-     * @param args the command line arguments
+     * This solves a given linear system of the form
+     * Ax = b
+     * A must be a square matrix with linearly independent rows.
+     * If there is no unique solution to the system, the results will be meaningless.
+     * If A is not square, a runtime exception will be thrown.
+     * @param input - The coefficient matrix of the linear system
+     * @param b - The right hand side of the equation for the linear system
+     * @return - The solution of the linear system (i.e. x) as a Matrix
+     * @throws SingularMatrixException
      */
-    
     public static Matrix solve(Matrix input, double[] b) throws SingularMatrixException
     {
     	//r1*c1*r2*c2 = r1*c2
