f0df7d39d6cf3eaa2d70f2ad1b38d4c0baa59cb7
Jacques Coetzee <jacqueslctz@gmail.com>
2013-10-23 21:25:19 +0200

    Added Settings unit test framework.

---
+++ EclipseProject/SketchupBlocks/test/sketchupblocks/base/SettingsTest.java
@@ -0,0 +1,24 @@
+package sketchupblocks.base;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SettingsTest {
+
+	@Test
+	public void testSettings() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSettingsString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testReadSettings() {
+		fail("Not yet implemented");
+	}
+
+}
